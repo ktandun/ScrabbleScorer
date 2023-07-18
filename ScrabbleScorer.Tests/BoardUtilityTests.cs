f86c1bb0f3576bbd787146d7c9f26ce2370f21b4
Kenzie Tandun <nudnateiznek@gmail.com>
2023-07-19 07:51:45 +1200

    add assertions

--- ScrabbleScorer.Tests/BoardUtilityTests.cs
+++ ScrabbleScorer.Tests/BoardUtilityTests.cs
@@ -35,7 +35,7 @@ public class BoardUtilityTests
 
         var words = BoardUtility.GetWordsOnBoard(board.BoardLetters);
     }
-    
+
     [Test]
     public void FindCreatedWordsAfterPlacementTest()
     {
@@ -51,7 +51,7 @@ public class BoardUtilityTests
                 new() { Letter = Letter.A, Coordinate = new Coordinate(8, 3) },
             }
         };
-        
+
         var after = new Board
         {
             BoardLetters = new BoardLetter[]
@@ -62,22 +62,85 @@ public class BoardUtilityTests
                 new() { Letter = Letter.A, Coordinate = new Coordinate(8, 5) },
                 new() { Letter = Letter.T, Coordinate = new Coordinate(8, 4) },
                 new() { Letter = Letter.A, Coordinate = new Coordinate(8, 3) },
-                
                 new() { Letter = Letter.P, Coordinate = new Coordinate(9, 8) },
                 new() { Letter = Letter.O, Coordinate = new Coordinate(10, 8) },
                 new() { Letter = Letter.O, Coordinate = new Coordinate(11, 8) },
                 new() { Letter = Letter.N, Coordinate = new Coordinate(12, 8) },
-                
                 new() { Letter = Letter.T, Coordinate = new Coordinate(10, 7) },
                 new() { Letter = Letter.H, Coordinate = new Coordinate(10, 6) },
                 new() { Letter = Letter.E, Coordinate = new Coordinate(10, 5) },
                 new() { Letter = Letter.R, Coordinate = new Coordinate(10, 4) },
-                
                 new() { Letter = Letter.O, Coordinate = new Coordinate(9, 7) },
                 new() { Letter = Letter.O, Coordinate = new Coordinate(9, 6) },
             }
         };
 
-        var words = BoardUtility.FindCreatedWordsAfterPlacement(before.BoardLetters, after.BoardLetters);
+        var words = BoardUtility.FindCreatedWordsAfterPlacement(
+            before.BoardLetters,
+            after.BoardLetters
+        );
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(
+                words.Contains(
+                    new WordPlacementModel
+                    {
+                        Word = "SPOON",
+                        Alignment = Alignment.Horizontal,
+                        Coordinate = new Coordinate(8, 8)
+                    }
+                ),
+                Is.True
+            );
+
+            Assert.That(
+                words.Contains(
+                    new WordPlacementModel
+                    {
+                        Word = "OTHER",
+                        Alignment = Alignment.Vertical,
+                        Coordinate = new Coordinate(10, 8)
+                    }
+                ),
+                Is.True
+            );
+
+            Assert.That(
+                words.Contains(
+                    new WordPlacementModel
+                    {
+                        Word = "POO",
+                        Alignment = Alignment.Vertical,
+                        Coordinate = new Coordinate(9, 8)
+                    }
+                ),
+                Is.True
+            );
+
+            Assert.That(
+                words.Contains(
+                    new WordPlacementModel
+                    {
+                        Word = "OOT",
+                        Alignment = Alignment.Horizontal,
+                        Coordinate = new Coordinate(8, 7)
+                    }
+                ),
+                Is.True
+            );
+
+            Assert.That(
+                words.Contains(
+                    new WordPlacementModel
+                    {
+                        Word = "NOH",
+                        Alignment = Alignment.Horizontal,
+                        Coordinate = new Coordinate(8, 6)
+                    }
+                ),
+                Is.True
+            );
+        });
     }
 }
